617661c1cd39fa59349776e783b61bd2388ac620
davidcoats <david@getsafetycone.com>
2023-03-27 23:04:08 -0700

    Initial commit.

---
+++ source/R5T.T0181.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0181.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.T0181 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0181/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0181
+{
+	/// <summary>
+	/// Common file type (EXE, XML, etc.) relative strong-types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
